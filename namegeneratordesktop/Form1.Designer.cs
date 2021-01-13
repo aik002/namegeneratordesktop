5bb6c1256e42acd0c157af98e3bbd1b13cafc358
Aiko Lebida <karafish3r@gmail.com>
2021-01-13 04:40:42 +0100

    basic layout and radio button selectors

--- namegeneratordesktop/Form1.Designer.cs
+++ namegeneratordesktop/Form1.Designer.cs
@@ -29,13 +29,151 @@ namespace namegeneratordesktop
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.radioButtonF = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonM = new System.Windows.Forms.RadioButton();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.uwu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButtonF
+            // 
+            this.radioButtonF.AutoSize = true;
+            this.radioButtonF.Location = new System.Drawing.Point(76, 145);
+            this.radioButtonF.Name = "radioButtonF";
+            this.radioButtonF.Size = new System.Drawing.Size(99, 24);
+            this.radioButtonF.TabIndex = 0;
+            this.radioButtonF.TabStop = true;
+            this.radioButtonF.Text = "Feminine";
+            this.radioButtonF.UseVisualStyleBackColor = true;
+            this.radioButtonF.CheckedChanged += new System.EventHandler(this.radioButtonF_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select gender:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // radioButtonM
+            // 
+            this.radioButtonM.AutoSize = true;
+            this.radioButtonM.Location = new System.Drawing.Point(76, 176);
+            this.radioButtonM.Name = "radioButtonM";
+            this.radioButtonM.Size = new System.Drawing.Size(105, 24);
+            this.radioButtonM.TabIndex = 2;
+            this.radioButtonM.TabStop = true;
+            this.radioButtonM.Text = "Masculine";
+            this.radioButtonM.UseVisualStyleBackColor = true;
+            this.radioButtonM.CheckedChanged += new System.EventHandler(this.radioButtonM_CheckedChanged);
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Location = new System.Drawing.Point(76, 207);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(111, 24);
+            this.radioButtonA.TabIndex = 3;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "Androgyne";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            this.radioButtonA.CheckedChanged += new System.EventHandler(this.radioButtonA_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(572, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(212, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(572, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(212, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Your new name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(572, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Usage Example:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 86);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Generate Name";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // uwu
+            // 
+            this.uwu.AutoSize = true;
+            this.uwu.Location = new System.Drawing.Point(956, 605);
+            this.uwu.Name = "uwu";
+            this.uwu.Size = new System.Drawing.Size(38, 20);
+            this.uwu.TabIndex = 9;
+            this.uwu.Text = "uwu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1074, 653);
+            this.Controls.Add(this.uwu);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButtonA);
+            this.Controls.Add(this.radioButtonM);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonF);
+            this.Name = "Form1";
+            this.Text = "Name Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonM;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label uwu;
     }
 }
 
--- namegeneratordesktop/Form1.cs
+++ namegeneratordesktop/Form1.cs
@@ -12,9 +12,54 @@ namespace namegeneratordesktop
 {
     public partial class Form1 : Form
     {
+        string gender;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Figure out selections
+            switch (gender)
+            {
+                case "f":
+                    textBox1.Text = "fem";
+                    break;
+
+                case "m":
+                    textBox1.Text = "masc";
+                    break;
+
+                case "a":
+                    textBox1.Text = "and";
+                    break;
+
+                default:
+                    textBox1.Text= "Unknown gender";
+                    break;
+            }
+
+        }
+
+        private void radioButtonF_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "f";
+        }
+
+        private void radioButtonM_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "m";
+        }
+
+        private void radioButtonA_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "a";
         }
     }
 }
